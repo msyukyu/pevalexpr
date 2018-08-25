8705269a34eb1e4b9a6f622062c4911debade8fa
Damien Abeloos <dabeloos@w-r3-p3.s19.be>
2018-08-25 13:20:55 +0200

    ft_opp

---
+++ ex00/ft_opp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_opp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 13:18:33 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/25 13:20:34 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_opp.h"
+
+int		ft_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int		ft_add(int a, int b)
+{
+	return (a + b);
+}
+
+int		ft_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int		ft_div(int a, int b)
+{
+	return (a / b);
+}
+
+int		ft_mod(int a, int b)
+{
+	return (a % b);
+}
